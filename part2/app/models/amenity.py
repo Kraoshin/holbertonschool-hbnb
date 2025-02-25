7e2c24886524e1e561d7f1571459d52d4b617000
Mame-Penda <mamependasadio1@gmail.com>
2025-02-25 11:10:48 +0100

    amenty class

--- part2/app/models/amenity.py
+++ part2/app/models/amenity.py
@@ -4,11 +4,11 @@ from basemodel import BaseModel
 class Amenity(BaseModel):
     """Represents an amenity in the hbnb app."""
 
-    def __init(self, name):
+    def __init__(self, name=""):
         """Initialize amenity with name."""
         super().__init__()
         self.name = name
 
     def __str__(self):
         """Return a string amenity."""
-        return (f"[Amenity] {self.name}")
+        return (f"[Amenity] ({self.id}) {self.name}")
