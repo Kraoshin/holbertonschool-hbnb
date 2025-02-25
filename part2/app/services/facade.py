169a73b95b7a5716ba1440683ea4c9671aed393a
kraoshin <killian.kerleau@outlook.fr>
2025-02-25 11:46:38 +0100

    change a lot of indentations

--- part2/app/services/facade.py
+++ part2/app/services/facade.py
@@ -1,6 +1,7 @@
 from app.persistence.repository import InMemoryRepository
 from app.models.review import Review
 
+
 class HBnBFacade:
     def __init__(self):
         self.user_repo = InMemoryRepository()
@@ -18,17 +19,17 @@ class HBnBFacade:
         # Logic will be implemented in later tasks
         pass
 
-    #Review facade
+#Review facade
     def create_review(self, review_data):
         review = Review(**review_data)
         self.review_repo.add(review)
         return review
 
     def get_review(self, review_id):
-       review = self.review_repo.get(review_id)
-       if not review:
-           raise ValueError("Review not found, please enter a valid review title")
-       return review
+        review = self.review_repo.get(review_id)
+        if not review:
+            raise ValueError("Review not found, please enter a valid review title")
+        return review
 
     def get_all_reviews(self):
         return self.review_repo.get_all()
@@ -38,11 +39,11 @@ class HBnBFacade:
         if not place:
             return "Place not found, please enter a valid place"
         return [review for review in self.review_repo.get_all() if review.place == place_id]
-    
+
     def update_review(self, review_id, review_update):
         review = self.review_repo.get(review_id)
         if not review:
-            return("Review not found, please enter a valid review title")
+            return ("Review not found, please enter a valid review title")
         for key, value in review_update.items():
             if hasattr(review, key):
                 setattr(review, key, value)
@@ -52,7 +53,6 @@ class HBnBFacade:
     def delete_review(self, review_id):
         review = self.review_repo.get(review_id)
         if not review:
-            return("Review not found, please enter a valid review title")
+            return ("Review not found, please enter a valid review title")
         self.review_repo.delete(review_id)
         return {'message': 'Review deleted successfully'}
-        
