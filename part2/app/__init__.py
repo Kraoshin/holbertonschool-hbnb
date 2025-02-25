4ce666cc15a794cab0caa913c6eef74f1fb4cb7d
kraoshin <killian.kerleau@outlook.fr>
2025-02-25 13:29:36 +0100

    add the namespace for the review part of the project

--- part2/app/__init__.py
+++ part2/app/__init__.py
@@ -5,4 +5,8 @@ def create_app():
     app = Flask(__name__)
     api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API', doc='/api/v1/')
 
+    api.add_namespace(reviews_ns, path='/api/v1/reviews')
+
+    from app.api.v1.reviews import api as reviews_ns
+    
     return app
