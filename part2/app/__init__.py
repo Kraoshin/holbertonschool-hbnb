1b01d9cb9d8dda082b6f6d1515122e5bb42310a8
Cyprien <cypriengehu@example.com>
2025-02-25 09:49:15 +0100

    implement new function in facade anb update init

--- part2/app/__init__.py
+++ part2/app/__init__.py
@@ -5,4 +5,7 @@ def create_app():
     app = Flask(__name__)
     api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API', doc='/api/v1/')
 
+    from app.api.v1.places import places as places_ns
+    
+    api.add_namaspace(places_ns, path='/app/api/v1/places')
     return app
--- part2/app/services/facade.py
+++ part2/app/services/facade.py
@@ -1,4 +1,5 @@
 from app.persistence.repository import InMemoryRepository
+from app.models.place import Place
 
 class HBnBFacade:
     def __init__(self):
@@ -7,12 +8,34 @@ class HBnBFacade:
         self.review_repo = InMemoryRepository()
         self.amenity_repo = InMemoryRepository()
 
-    # Placeholder method for creating a user
-    def create_user(self, user_data):
-        # Logic will be implemented in later tasks
-        pass
+    #Place facade
 
-    # Placeholder method for fetching a place by ID
+    def create_place(self, place_data):
+        # the function crrate a new place with the data we provided
+        new_place = Place(**place_data)
+        self.place_repo.add(new_place)
+        return new_place
+
     def get_place(self, place_id):
-        # Logic will be implemented in later tasks
-        pass
+        # The function get a place with place_id
+        place = self.place_repo.get(place_id)
+        if not place:
+            raise ValueError("Place not found")
+        return place
+
+    def get_all_places(self):
+        # The function get all places in the repo
+        return self.place_repo.get_all()
+
+    def update_place(self, place_id, place_data):
+        # the function will update a place with new data
+        place = self.place_repo.get(place_id)
+        if not place:
+            raise ValueError("Place not found")
+        
+        for keys, value in place_data.items():
+            if hasattr(place, keys):
+                setattr(place, keys, value)
+        
+        self.place_repo.update(place_id, place.__dict__)
+        return place
