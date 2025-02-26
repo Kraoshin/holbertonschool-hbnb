7d46d4dab560df571a68d76cc091b4fd1f92c074
Mame-Penda <mamependasadio1@gmail.com>
2025-02-26 11:55:02 +0100

    facade amenities

--- part2/app/services/facade.py
+++ part2/app/services/facade.py
@@ -1,4 +1,6 @@
 from app.persistence.repository import InMemoryRepository
+from app.models.amenity import Amenity
+
 
 class HBnBFacade:
     def __init__(self):
@@ -16,3 +18,32 @@ class HBnBFacade:
     def get_place(self, place_id):
         # Logic will be implemented in later tasks
         pass
+
+    # Amenity Facades
+    def create_amenity(self, amenity_data):
+        """Create a new amenity"""
+        amenity = Amenity(**amenity_data)
+        self.amenity_repo.add(amenity)
+        return amenity
+
+    def get_amenity(self, amenity_id):
+        """Retrieve an amenity by ID"""
+        return self.amenity_repo.get(amenity_id)
+
+    def get_all_amenities(self):
+        """Retrieve all amenities"""
+        return self.amenity_repo.get_all()
+
+    def update_amenity(self, amenity_id, amenity_data):
+        """Update an amenity by ID"""
+        amenity = self.get_amenity(amenity_id)
+        if not amenity:
+            raise ValueError("Amenity not found")
+
+        for key, value in amenity_data.items():
+            if hasattr(amenity, key):
+                setattr(amenity, key, value)
+
+                self.amenity_repo.update(amenity_id, amenity.__dict__)
+
+                return amenity
