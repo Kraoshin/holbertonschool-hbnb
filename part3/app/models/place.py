c20a421f54bff321b5f937aa3a6d30959339d287
kraoshin <killian.kerleau@outlook.fr>
2025-03-12 16:03:31 +0100

    test to see if i can push

--- part3/app/models/place.py
+++ part3/app/models/place.py
@@ -1,29 +1,35 @@
-from .basemodel import BaseModel
-from .user import User
-from .amenity import Amenity
+from app.models.base_model import BaseModel
+from app.models.user import User
+from app import db
+from sqlalchemy.orm import relationship
+from app.models.place_amenity import place_amenity
 
 
 class Place(BaseModel):
-    def __init__(self, title, description, price, latitude,
-                 longitude, owner_id, amenities=None):
-        super().__init__()
-        self.title = title
-        self.description = description
-        self.price = price
-        self.latitude = latitude
-        self.longitude = longitude
-        self.owner_id = owner_id
-        self.reviews = []
-        self.amenities = amenities or []
+    """Place class that inherits from BaseModel."""
+   
+    __tablename__ = 'places'
+
+    title = db.Column(db.String(36), nullable=False)
+    description = db.Column(db.String(), nullable=False)
+    price = db.Column(db.Float(), nullable=False)
+    latitude = db.Column(db.Float(), nullable=False)
+    longitude = db.Column(db.Float(), nullable=False)
+    owner_id = db.Column(db.String(36), db.ForeignKey('users.id'), nullable=False)
+    reviews = db.relationship('Review', backref='place', lazy=True)
+    amenities = db.relationship('Amenity', secondary=place_amenity, lazy='subquery', backref=db.backref('places', lazy=True))
 
     def add_review(self, review):
         """Add a review to the place."""
         self.reviews.append(review)
 
+    def remove_review(self, review):
+        """Remove a review from the place."""
+        if review in self.reviews:
+            self.reviews.remove(review)
+
     def add_amenity(self, amenity):
         """Add an amenity to the place."""
-        if not isinstance(amenity, Amenity):
-            raise ValueError("amenity must be an Amenity instance")
         self.amenities.append(amenity)
 
     @property
