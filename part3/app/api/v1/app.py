6f13eae3c913cdb80ceb2c440ed8f16df5fcc168
Mame-Penda <mamependasadio1@gmail.com>
2025-03-12 14:04:31 +0100

    app configuration

--- part3/app/api/v1/app.py
+++ part3/app/api/v1/app.py
@@ -2,7 +2,8 @@ from flask import Flask
 from flask_restx import Api
 from flask_jwt_extended import JWTManager
 from app.routes.auth import auth_api
-from routes.auth import auth_api
+from api.v1.auth import auth_api
+
 
 
 app = Flask(__name__)
