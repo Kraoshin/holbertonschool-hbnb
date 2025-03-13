fcf4ad799fac53b25d55d63082e6162e91ff0633
Cyprien <cypriengehu@example.com>
2025-03-13 22:07:54 +0100

    some correction in revie part and admin part

--- part3/app/models/review.py
+++ part3/app/models/review.py
@@ -8,11 +8,10 @@ from sqlalchemy.orm import validates
 class Review(BaseModel):
     __tablename__ = 'reviews'
 
-    id = db.Column(db.Integer, primary_key=True)
     text = db.Column(db.String(1024), nullable=False)
     rating = db.Column(db.Integer, nullable=False)
-    place_id = db.Column(db.Integer, db.ForeignKey('places.id'), nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    place_id = db.Column(db.String(36), db.ForeignKey('places.id'), nullable=False)
+    user_id = db.Column(db.String(36), db.ForeignKey('users.id'), nullable=False)
 
     @validates('text')
     def validate_text(self, key, value):
