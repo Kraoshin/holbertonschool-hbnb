41e616ff68563849635bdb1ee739f6763505f8f9
kraoshin <killian.kerleau@outlook.fr>
2025-03-13 16:00:43 +0100

    aze

--- part3/app/models/relationship.py
+++
@@ -1,7 +0,0 @@
-from sqlalchemy import Table, Column, Integer, ForeignKey
-from app import db
-
-place_amenity = Table('place_amenity', db.Model.metadata,
-                      Column('place_id', Integer, ForeignKey('places.id'), primary_key=True),
-                      Column('amenity_id', Integer, ForeignKey('amenities.id'), primary_key=True)
-)
