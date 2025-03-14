ab260091c5aa40bcb6b0eac686497b487063b798
kraoshin <killian.kerleau@outlook.fr>
2025-03-14 18:16:36 +0100

    modified validates method to be more precise

--- part3/app/models/user.py
+++ part3/app/models/user.py
@@ -1,7 +1,6 @@
 from app import db, bcrypt
 from .basemodel import BaseModel
-from sqlalchemy.orm import validates
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import validates, relationship
 import re
 
 class User(BaseModel):     
@@ -12,8 +11,7 @@ class User(BaseModel):
     email = db.Column(db.String(120), nullable=False, unique=True)
     password = db.Column(db.String(128), nullable=False)
     is_admin = db.Column(db.Boolean, default=False)
-    places = relationship('Place', backref='owner', lazy=True)
-    reviews = relationship('Review', backref='author', lazy=True)
+    places = relationship('Place', backref='users', lazy=True)
     
     def verify_password(self, password):
         """Verifies if the provided password matches the hashed password"""
@@ -30,6 +28,18 @@ class User(BaseModel):
             return {"error": "Invalid email format"}
         return value 
     
-    @validates("password")
-    def validate_password(self, key, value):
+    @validates('first_name', 'last_name')
+    def validate_first_name(self, key, value):
+        if not isinstance(value, str):
+            raise TypeError("{} must be a string".format(key.replace("_", " ")).capitalize())
+        if len(value.replace(" ", "")) < 2 or len(value.replace(" ", "")) > 50:
+            raise ValueError("{} must have between 2 and 50 characters".format(key.replace("_", " ")).capitalize())
         return value
+    
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "first_name": self.first_name,
+            "last_name": self.last_name,
+            "email": self.email
+        }
