32bc272b7a26280238ae9868588a9d6636930b00
kraoshin <killian.kerleau@outlook.fr>
2025-03-14 18:18:07 +0100

    modified validates method to be more precise

--- part3/app/models/review.py
+++ part3/app/models/review.py
@@ -6,32 +6,35 @@ from sqlalchemy.orm import validates
 class Review(BaseModel):
     __tablename__ = 'reviews'
 
-    text = db.Column(db.String(150), nullable=False)
     rating = db.Column(db.Integer, nullable=False)
-    user_id = db.Column(db.String(36), db.ForeignKey('users.id'), nullable=False)
-    place_id = db.Column(db.String(36), db.ForeignKey('places.id'), nullable=False)
+    text = db.Column(db.Text, nullable=False)
+    place_id = db.Column(db.Integer, db.ForeignKey('places.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     
 
     @validates('text')
     def validate_text(self, key, value):
-        if not value and len(value) > 150:
-            raise ValueError("Text must be less than 50 characters")
+        if not isinstance(value, str):
+            raise TypeError("Text is not valid")
+        if not value:
+            raise TypeError("Text is required")
         return value
 
     @validates('rating')
     def validate_rating(self, key, value):
-        if not value or not (1 <= value <= 5):
+        if not value:
+            raise TypeError("Rating is required")
+        if not isinstance(value, int):
+            raise TypeError("Rating is not valid")
+        if value < 1 or value > 5:
             raise ValueError("Rating must be between 1 and 5")
         return value
-
-    @validates('user_id')
-    def validate_user_id(self, key, value):
-        if not value or not isinstance(value, str):
-            raise ValueError("User not found, please enter a valid username")
-        return value
-
-    @validates('place_id')
-    def validate_place_id(self, key, value):
-        if not value or not isinstance(value, str):
-            raise ValueError("Place not found, please enter a valid place")
-        return value
+    
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "text": self.text,
+            "rating": self.rating,
+            "user_id": self.user_id,
+            "place_id": self.place_id
+        }
